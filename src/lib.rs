63721a06602c1e8c42c0e4504d1cf7c7facf84d1
franRappazzini <fran_rappa@outlook.com>
2024-07-30 19:23:01 -0300

    feat: shared state

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,30 @@
-use std::{collections::HashMap, fs};
+use std::{
+    collections::HashMap,
+    fs,
+    sync::{Arc, Mutex},
+};
 
 use axum::{
-    extract::{Path, Query},
+    extract::{Path, Query, State},
+    http::{HeaderMap, Method},
     Json,
 };
 use serde_json::{json, Value};
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub counter: Arc<Mutex<u8>>,
+    pub v: Arc<Mutex<Vec<String>>>,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        Self {
+            counter: Arc::new(Mutex::new(0)),
+            v: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
+}
 
 pub async fn index() -> String {
     // "Hello, World!"
@@ -47,4 +67,35 @@ pub async fn json(Json(payload): Json<serde_json::Value>) {
 // `Json` gives a content-type of `application/json` and works with any type that implements `serde::Serialize`
 pub async fn res_json() -> Json<Value> {
     Json(json!({"data":123, "messsage": "hola que tal, este es un json"}))
+}
+
+// -----------
+// shared states
+// -----------
+
+#[derive(Debug)]
+struct BodyJson {
+    json: String,
+    name: String,
+    age: u128,
+}
+pub async fn handler_state(State(state): State<AppState>) -> String {
+    let counter = state.counter.lock().expect("lock counter");
+    let v = state.v.lock().expect("lock v");
+
+    println!("coutner: {}. vector: {:#?}", counter, v);
+
+    // println!("coutner: {}. vector: {:#?}", state.counter, state.v);
+    format!("coutner: {}. vector: {:#?}", counter, v)
+}
+
+pub async fn handler_state_post(State(state): State<AppState>) -> Json<Value> {
+    let mut counter = state.counter.lock().expect("lock counter");
+    *counter += 1;
+    let mut v = state.v.lock().expect("lock v");
+    v.push(format!("se agrego {}", counter));
+
+    println!("coutner: {}. vector: {:#?}", counter, v);
+
+    Json(json!({"counter": *counter, "vector": *v}))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use axum::{routing::get, Router};
-use axum_server::{get_foo, index, post_foo, res_json, user_get};
+use axum_server::{
+    get_foo, handler_state, handler_state_post, index, post_foo, res_json, user_get, AppState,
+};
 
 #[tokio::main]
 async fn main() {
+    // shared state
+    let shared_state = AppState::new();
+
     // build our application with a single route
     let app = Router::new()
         .route("/", get(index))
         .route("/foo", get(get_foo).post(post_foo))
         .route("/user/{user_id}", get(user_get))
-        .route("/json", get(res_json));
+        .route("/json", get(res_json))
+        .route("/shared_state", get(handler_state).post(handler_state_post))
+        .with_state(shared_state);
 
     let host = std::env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
     let port = std::env::var("PORT").unwrap_or_else(|_| "7878".to_string());
